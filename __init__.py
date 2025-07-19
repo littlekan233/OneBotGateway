0f061dfb3c9fe4631cc64b45b01df4b8cf32fb76
littlekan233 <little-kan@qq.com>
2025-07-19 09:06:23 +0800

    delete useless things

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-from dotenv import load_dotenv
-from os import getenv
-from sys import stdout
-
-logger.remove(0)
-logger.add(stdout, level=getenv('LOGLEVEL', 'info').upper(), format='<magenta>[{time:HH:mm:ss}]</magenta> <orange>')
